563b489f17e85274504b35348003173953427921
danTapps <daniel.terryn@gmail.com>
2019-06-08 15:26:41 -0400

    Update parent.groovy

--- Drivers/TED Pro Home/parent.groovy
+++ Drivers/TED Pro Home/parent.groovy
@@ -1,3 +1,24 @@
+/*
+ * TED Pro Home
+ *
+ *  Copyright 2019 Daniel Terryn
+ *
+ *  Licensed Virtual Image Switch the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Change History:
+ *
+ *    Date        Who            What
+ *    ----        ---            ----
+ *    2019-05-21  Daniel Terryn  Original Creation
+ * 
+ */
 metadata {
     definition (name: "TED Pro Home", author: "dan.t", namespace: "dan.t") {
         capability "Polling"
