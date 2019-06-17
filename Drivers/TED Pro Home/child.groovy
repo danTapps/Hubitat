940eb8e07c3c355c780eccf8b2b86ac98e9d03a0
danTapps <daniel.terryn@gmail.com>
2019-06-17 09:41:48 -0400

    Update child.groovy

--- Drivers/TED Pro Home/child.groovy
+++ Drivers/TED Pro Home/child.groovy
@@ -3,7 +3,7 @@
  *
  *  Copyright 2019 Daniel Terryn
  *
- *  Licensed Virtual Image Switch the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
