130034c8aa6545adaad2d687ea4bd4d36d0230db
Daniel Terryn <Daniel.Terryn@gmail.com>
2019-09-23 14:01:19 -0400

    Send event when time set, fixed NTP time calculation, more choices for time difference configuration, refactoring, show NTP Server Date as Event, add force command

--- Drivers/NTP Client/NTP_client.groovy
+++ Drivers/NTP Client/NTP_client.groovy
@@ -3,9 +3,9 @@
  *
  *  Name: NTP Client to retrieve Date/Time from local NTP server....
  *
- *  Date: 2019-09-22
+ *  Date: 2019-09-23
  *
- *  Version: 1.10
+ *  Version: 1.20
  *
  *  Author: Daniel Terryn
  *
@@ -20,19 +20,36 @@
  *   Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *   for the specific language governing permissions and limitations under the License.
- *****************************************************************************************************************/
+ *****************************************************************************************************************
+ *  Change History:
+ *
+ *    Date        Who            What
+ *    ----        ---            ----
+ *    2019-09-22  Daniel Terryn  Original Creation
+ *    2019-09-23  Daniel Terryn  Send event when time set, fixed NTP time calculation, more choices for time difference configuration, refactoring, show NTP Server Date as Event, add force command
+ *
+ * 
+ */
 
+double SECONDS_1900_TO_EPOCH() {return 2208988800.0 as double}
 
 metadata {
     definition (name: "NTP Client", author: "dan.t", namespace: "dan.t", importUrl: "https://raw.githubusercontent.com/danTapps/Hubitat/master/Drivers/NTP%20Client/NTP_client.groovy") {
         capability "Actuator"
-        capability "Refresh"
         capability "Initialize"
+        capability "Refresh"
         
+        attribute "lastNTPdate", "string"
+        attribute "lastHubDate", "string"
+        attribute "lastDiffMS", "number"
+        attribute "updateHubTimeTo", "string"
+        
+        command "force"
+
     }
     preferences {
-        input ( name: "ip", type: "text", title: "NTP Server IP Address", description: "IP Address in form 192.168.1.226", required: true, displayDuringSetup: true )
-        input ( name: "port", type: "text", title: "NTP Server Port", description: "port in form of 123", required: true, displayDuringSetup: true, default: 123 )
+        input ( name: "ntpIP", type: "text", title: "NTP Server IP Address", description: "IP Address in form 192.168.1.226", required: true, displayDuringSetup: true )
+        input ( name: "ntpPort", type: "text", title: "NTP Server Port", description: "port in form of 123", required: true, displayDuringSetup: true, default: 123 )
         input ( name: "minTimeDiff", title: "Minimum time difference between Hub and NTP Server to update time.", type: "enum",
             options: [
                 180000 : "3 Minutes",
@@ -49,7 +66,7 @@ metadata {
             displayDuringSetup: true, required: true )   
         input ( name: 'pollInterval', type: 'enum', title: 'Update interval (in minutes)', options: ['10', '30', '60', '120', '300'], required: true, displayDuringSetup: true )
 
-        input ( name: "configLoggingLevelIDE", title: "IDE Live Logging Level:\nMessages with this level and higher will be logged to the IDE.", type: "enum",
+        input ( name: "configLoggingLevel", title: "Live Logging Level:\nMessages with this level and higher will be logged.", type: "enum",
             options: [
                 "0" : "None",
                 "1" : "Error",
@@ -81,7 +98,7 @@ def updated() {
 
 def configure() {
     logger("Executing 'configure()'", "info")
-    state.loggingLevelIDE = (settings.configLoggingLevelIDE) ? settings.configLoggingLevelIDE.toInteger() : 3
+    state.loggingLevel = (configLoggingLevel) ? configLoggingLevel.toInteger() : 3
     updateDeviceNetworkID()
 
     unschedule()
@@ -97,29 +114,68 @@ def parse(description) {
     try {
         def encrResponse = parseLanMessage(description).payload
         byte[] rawBytes = hubitat.helper.HexUtils.hexStringToByteArray(encrResponse);
-        def newTimeMS = getNTPTimeMS(rawBytes, now())
-        def newDate = new Date(newTimeMS.toLong())
+        def hubTimeMS = now() 
+        def newTimeMS = getNTPTimeMS(rawBytes, hubTimeMS)
         
         logger("NTP Server returned time of ${newTimeMS} aka ${new Date(newTimeMS.toLong())}", "info")
-        logger("Hub is ${now()} aka ${new Date(now())}", "info")
-        def timeDiff = newTimeMS - now() as long
+        logger("Hub is ${hubTimeMS} aka ${new Date(hubTimeMS)}", "info")
+        def timeDiff = newTimeMS - hubTimeMS as long
         if (timeDiff < 0)
             timeDiff = timeDiff * -1
         logger("Time Diff is ${timeDiff} ms", "debug")
         logger("minTimeDiff is ${minTimeDiff} ms", "debug")
-        if (timeDiff >= Long.parseLong(minTimeDiff))
+        sendEvent(name: "lastNTPdate", value: (new Date(newTimeMS.toLong())).toString())
+        sendEvent(name: "lastHubDate", value: (new Date(hubTimeMS)).toString())
+        sendEvent(name: "lastDiffMS", value: timeDiff)
+
+        if ((timeDiff >= Long.parseLong(minTimeDiff)) || (state?.force == true))
         {
-            logger("Update Hub Time to ${newDate}", "info")
+            if (state?.force == true)
+                logger("Force Update Hub Time to ${(new Date(newTimeMS.toLong())).toString()}", "info")
+            else
+                logger("Update Hub Time to ${(new Date(newTimeMS.toLong())).toString()}", "info")
+            sendEvent(name: "updateHubTimeTo", value: (new Date(newTimeMS.toLong())).toString())
             location.hub.updateSystemTime(new Date(newTimeMS.toLong()))
         }
+        state.force = false
 
     } catch (error) {
-        logger("${error}", "warn")
+        logger("<font color=red>${error}</font>", "warn")
     }
+}
+
+def refresh() {
+    //def SECONDS_1900_TO_EPOCH = 2208988800.0 as double
+
+    logger("Executing 'refresh()'", "debug")
+    byte[] rawBytes = [0x1b, 0x0,  0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 
+                       0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 
+                       0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 
+                       0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 
+                       0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0]
+    rawBytes = encodeTimestamp(rawBytes, 40, (now()/1000)+SECONDS_1900_TO_EPOCH())
+
+    String stringBytes = hubitat.helper.HexUtils.byteArrayToHexString(rawBytes)
+    def myHubAction = new hubitat.device.HubAction(stringBytes, 
+                           hubitat.device.Protocol.LAN, 
+                           [type: hubitat.device.HubAction.Type.LAN_TYPE_UDPCLIENT, 
+                            destinationAddress: "${ntpIP}:${ntpPort}",
+                            encoding: hubitat.device.HubAction.Encoding.HEX_STRING])
+    sendHubCommand(myHubAction)
+}
+
+def force()
+{
+    logger("Executing 'force()'", "debug")
+    state.force = true
+    logger("<b>Forcing</b> Date/Time update in 2 seconds", "info")
+    runIn(2, "refresh", [overwrite: true])
+
 }
 
 def getTimestamp(byte[] array, int pointer)
 {
+    logger("Executing 'getTimestamp()'", "debug")
     def r = 0.0 as double
         
     for(int i=0; i<8; i++)
@@ -136,20 +192,9 @@ short unsignedByteToShort(byte b)
     else return (short) b
 }
 
-def timestampToMS (timestamp) {
-
-    def DAYS = 25567 as long; // 1 Jan 1900 to 1 Jan 1970
-    def SECS = 60 * 60 * 24 * DAYS as long
-    if(timestamp==0) return 0
-    
-    // timestamp is relative to 1900, utc is used by Java and is relative to 1970 
-    return Math.round (1000.0*(timestamp-SECS))
-}
-
-def getNTPTimeMS(byte[] array, destinationTimestamp) {
-    def SECONDS_1900_TO_EPOCH = 2208988800.0 as double
-
-    destinationTimestamp = (destinationTimestamp/1000) + SECONDS_1900_TO_EPOCH
+def getNTPTimeMS(byte[] array, destinationTimestamp) 
+{
+    logger("Executing 'getNTPTimeMS()'", "debug")
     // See the packet format diagram in RFC 2030 for details 
     
     /* 
@@ -182,84 +227,59 @@ def getNTPTimeMS(byte[] array, destinationTimestamp) {
     receiveTimestamp = getTimestamp(array, 32)
     transmitTimestamp = getTimestamp(array, 40)
 
-    double localClockOffset = ((receiveTimestamp - originateTimestamp) +
-                               (transmitTimestamp - destinationTimestamp)) / 2;
-    
-    return (now() + localClockOffset*1000)
+    return (now() + (((receiveTimestamp - originateTimestamp) + (transmitTimestamp - ((destinationTimestamp/1000) + SECONDS_1900_TO_EPOCH()))) / 2)*1000)
 }
 
 def encodeTimestamp(array,pointer, timestamp)
 {
-        // Converts a double into a 64-bit fixed point
-        for(i=0; i<8; i++) {
-            // 2^24, 2^16, 2^8, .. 2^-32
-            double base = Math.pow(2, (3-i)*8);
+    logger("Executing 'encodeTimestamp()'", "debug")
+    // Converts a double into a 64-bit fixed point
+    for(i=0; i<8; i++) {
+        // 2^24, 2^16, 2^8, .. 2^-32
+        double base = Math.pow(2, (3-i)*8);
                
-            // Capture byte value
-            array[pointer+i] = (byte) (timestamp / base);
+        // Capture byte value
+        array[pointer+i] = (byte) (timestamp / base);
 
-            // Subtract captured value from remaining total
-            timestamp = timestamp - (unsignedByteToShort(array[pointer+i]) * base);
-        }
+        // Subtract captured value from remaining total
+        timestamp = timestamp - (unsignedByteToShort(array[pointer+i]) * base);
+    }
 
-        // From RFC 2030: It is advisable to fill the non-significant
-        // low order bits of the timestamp with a random, unbiased
-        // bitstring, both to avoid systematic roundoff errors and as
-        // a means of loop detection and replay detection.
-        //array[7+pointer] = (byte) (RandomSource.getInstance().nextInt());
-        array[7+pointer] = 0x0;
+    array[7+pointer] = 0x0;
     return array;
 }
 
-def refresh() {
-    def SECONDS_1900_TO_EPOCH = 2208988800.0 as double
-
-    logger("Executing 'refresh()'", "debug")
-    byte[] rawBytes = [0x1b, 0x0,  0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 
-                       0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 
-                       0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 
-                       0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 
-                       0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0]
-    rawBytes = encodeTimestamp(rawBytes, 40, (now()/1000)+SECONDS_1900_TO_EPOCH)
-
-    String stringBytes = hubitat.helper.HexUtils.byteArrayToHexString(rawBytes)
-    def myHubAction = new hubitat.device.HubAction(stringBytes, 
-                           hubitat.device.Protocol.LAN, 
-                           [type: hubitat.device.HubAction.Type.LAN_TYPE_UDPCLIENT, 
-                            destinationAddress: "${settings.ip}:${settings.port}",
-                            encoding: hubitat.device.HubAction.Encoding.HEX_STRING])
-    sendHubCommand(myHubAction)
-}
 
 def getHostAddress() {
-    def ip = settings.ip
-    def port = settings.port
-    
-    logger("Using ip: ${ip} and port: ${port} for device: ${device.id}", "debug")
-    return ip + ":" + port
+    logger("Executing 'getHostAddress()'", "debug")
+    logger("Using ip: ${ntpIP} and port: ${ntpPort} for device: ${device.id}", "debug")
+    return ntpIP + ":" + ntpPort
 }
 
 def updateDeviceNetworkID() {
     logger("Executing 'updateDeviceNetworkID'", "debug")
     
-    if(device.deviceNetworkId!=getDNIfromIPandPort(settings.ip, settings.port)) {
-        logger("setting deviceNetworkID = ${getDNIfromIPandPort(settings.ip, settings.port)}", "debug")
-        device.setDeviceNetworkId(getDNIfromIPandPort(settings.ip, settings.port))
+    if(device.deviceNetworkId!=getDNIfromIPandPort(ntpIP, ntpPort)) {
+        logger("setting deviceNetworkID = ${getDNIfromIPandPort(ntpIP, ntpPort)}", "debug")
+        device.setDeviceNetworkId(getDNIfromIPandPort(ntpIP, ntpPort))
     }
 }
 
 def getDNIfromIPandPort(ipAddress, port)
 {
+    logger("Executing 'getDNIfromIPandPort'", "debug")
     def iphex = convertIPtoHex(ipAddress)
     def porthex = convertPortToHex(port)
 
     return "${iphex}:${porthex}"
 }
 def convertIPtoHex(ipAddress) { 
+    logger("Executing 'convertIPtoHex'", "debug")
     String hex = ipAddress.tokenize( '.' ).collect {  String.format( '%02x', it.toInteger() ) }.join()
     return hex
 }
 def convertPortToHex(port) {
+    logger("Executing 'convertPortToHex'", "debug")
     String hexport = port.toString().format( '%04x', port.toInteger() )
     return hexport
 }
@@ -272,23 +292,23 @@ private logger(msg, level = "debug") {
 
     switch(level) {
         case "error":
-            if (state.loggingLevelIDE >= 1) log.error msg
+            if (state.loggingLevel >= 1) log.error msg
             break
 
         case "warn":
-            if (state.loggingLevelIDE >= 2) log.warn msg
+            if (state.loggingLevel >= 2) log.warn msg
             break
 
         case "info":
-            if (state.loggingLevelIDE >= 3) log.info msg
+            if (state.loggingLevel >= 3) log.info msg
             break
 
         case "debug":
-            if (state.loggingLevelIDE >= 4) log.debug msg
+            if (state.loggingLevel >= 4) log.debug msg
             break
 
         case "trace":
-            if (state.loggingLevelIDE >= 5) log.trace msg
+            if (state.loggingLevel >= 5) log.trace msg
             break
 
         default:
